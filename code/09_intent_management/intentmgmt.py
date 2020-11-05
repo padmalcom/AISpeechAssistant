89a247da49ab2e3c7299e30d4adbea4d06025b1d
padmalcom <j.freiknecht@googlemail.com>
2020-11-05 22:39:51 +0100

    worked on intent management

--- code/09_intent_management/intentmgmt.py
+++ code/09_intent_management/intentmgmt.py
@@ -1,7 +1,13 @@
 import pip
 import importlib
+import importlib.util
 import glob
 import os
+import sys
+from pathlib import Path
+import json
+from snips_nlu import SnipsNLUEngine
+from snips_nlu.default_configs import CONFIG_DE
 
 class IntentMgmt:
 
@@ -23,26 +29,46 @@ class IntentMgmt:
 		return 1
 
 	def __init__(self):
-		self.snips_nlu_folders = [os.path.abspath(name) for name in glob.glob("./intents/snips-nlu/*/")]
 		self.chatbotai_folders = [os.path.abspath(name) for name in glob.glob("./intents/chatbotai/*/")]
 		self.functions_folders = [os.path.abspath(name) for name in glob.glob("./intents/functions/*/")]
 		
-		# install functions
+		# Registriere Funktionen, die von snips-nlu und chatbotai aufgerufen werden
+		function_count = 0
 		for ff in self.functions_folders:
 			print("Suche nach Funktionen in " + ff)
 			req_file = os.path.join(ff, 'requirements.txt')
 			if os.path.exists(req_file):
 				install_result = self.install_requirements(req_file)
-				print(install_result)
-				if install_result:
-					print("Abhängigkeiten für " + ff + " erfolgreich installiert.")
+				if install_result == 0:
+					print("Abhängigkeiten für " + ff + " erfolgreich installiert oder bereits vorhanden.")
 			
 			# Finde Python-Dateien, die mit Intent beginnen
 			intent_files = glob.glob(os.path.join(ff, 'intent_*.py'))
 			for infi in intent_files:
 				print("Lade Intent Datei " + infi)
-				new_module = importlib.import_module(infi)
-				print("Modul " + new_module + " geladen.")
+				spec = importlib.util.spec_from_file_location(Path(ff).name, infi)
+				new_module = importlib.util.module_from_spec(spec)
+				print("Modul " + str(new_module) + " geladen.")
+				function_count +=1
+				
+		# Trainiere snips-nlu
+		self.snips_nlu_engine = SnipsNLUEngine(Config=CONFIG_DE)
+		#json_content = ''
+		snips_nlu_count = 0
+		snips_files = glob.glob(os.path.join("./intents/snips-nlu", '*.json'))
+		for sf in snips_files:
+			print("Verarbeite snips nlu training datei " + sf)
+			with open(sf, 'r') as json_file:
+				json_content = json.load(json_file)
+				self.snips_nlu_engine = self.snips_nlu_engine.fit(json_content)
+				print(self.snips_nlu_engine.dataset_metadata)
+				snips_nlu_count +=1
+
+		print(str(snips_nlu_count) + " Snips NLU files gefunden.")
+
+		
+		print("Snips NLU Training abgeschlossen")
+					
 				
 	
 	def evaluate(input: str, output: str):
