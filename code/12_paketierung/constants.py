4136acb69ca35e3c3d837d3383f131dd6058f5b3
padmalcom <j.freiknecht@googlemail.com>
2021-01-08 21:24:43 +0100

    fixed chapter 12

--- code/12_paketierung/constants.py
+++
@@ -1,6 +0,0 @@
-# Konstanten für die Darstellung des Icons
-TRAY_TOOLTIP = 'Voice Assistant'
-TRAY_ICON_INITIALIZING = 'initializing.png'
-TRAY_ICON_IDLE = 'idle.png' 
-TRAY_ICON_LISTENING = 'listening.png'
-TRAY_ICON_SPEAKING = 'speaking.png'
--- code/12_paketierung/global_variables.py
+++
@@ -1,1 +0,0 @@
-voice_assistant = None
