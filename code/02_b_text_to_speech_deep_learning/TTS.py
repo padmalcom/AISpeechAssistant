9eebc0d835fc71282948d3522a60b7c3d2e3f3d1
padmalcom <j.freiknecht@googlemail.com>
2021-08-26 14:52:56 +0200

    Error Handling in Deep Learning TTS Beispiel hinzugefügt. Modelle für Vocoder und Encoder beigelegt.

--- code/02_b_text_to_speech_deep_learning/TTS.py
+++ code/02_b_text_to_speech_deep_learning/TTS.py
@@ -27,25 +27,47 @@ class Voice:
 		self.process = None
 		self.griffin_lim = fast_vocoding
 
-		enc_model_fpath = Path("encoder/saved_models/my_run.pt")
-		syn_model_fpath = Path("synthesizer/saved_models/my_run/my_run.pt")
-		voc_model_fpath = Path("vocoder/saved_models/my_run/my_run.pt")
-		encoder.load_model(enc_model_fpath)
-		self.synthesizer = Synthesizer(syn_model_fpath)
-		vocoder.load_model(voc_model_fpath)
+		self.enc_model_fpath = Path("encoder/saved_models/my_run.pt")
+		self.syn_model_fpath = Path("synthesizer/saved_models/my_run/my_run.pt")
+		self.voc_model_fpath = Path("vocoder/saved_models/my_run/my_run.pt")
+		
+		if self.enc_model_fpath.exists():
+			encoder.load_model(self.enc_model_fpath)
+		else:
+			logger.warning("Encoder-Model existiert nicht. Bitte runterladen von https://github.com/padmalcom/Real-Time-Voice-Cloning-German/releases.")
+			
+		if self.syn_model_fpath.exists():
+			self.synthesizer = Synthesizer(self.syn_model_fpath)
+		else:
+			logger.error("Synthesizer-Model existiert nicht. Bitte runterladen von https://github.com/padmalcom/Real-Time-Voice-Cloning-German/releases.")
+			
+		if self.voc_model_fpath.exists():
+			vocoder.load_model(self.voc_model_fpath)
+		else:
+			logger.warning("Vocoder-Model existiert nicht. Bitte runterladen von https://github.com/padmalcom/Real-Time-Voice-Cloning-German/releases.")
 		
 	def __speak__(self, text):
 		texts = [text]
 		embeds = [[0] * 256]
 		
+		if not self.syn_model_fpath.exists():
+			logger.error("Synthesizer-Modell ist nicht geladen. TTS fehlgeschlagen.")
+			return
+			
 		specs = self.synthesizer.synthesize_spectrograms(texts, embeds)
 		spec = specs[0]
 	
-		if not self.griffin_lim:
+		if not self.griffin_lim and self.voc_model_fpath.exists():
 			generated_wav = vocoder.infer_waveform(spec)
 			generated_wav = np.pad(generated_wav, (0, self.synthesizer.sample_rate), mode="constant")
-			generated_wav = encoder.preprocess_wav(generated_wav)
+			
+			if self.enc_model_fpath.exists():
+				generated_wav = encoder.preprocess_wav(generated_wav)
+			else:
+				logger.warning("Kann Ausgabe nicht bereinigen, da Encoder-Modell nicht geladen werden kann.")
 		else:
+			if not self.voc_model_fpath.exists():
+				logger.warning("Vocoder-Model existiert nicht. Fallback zu Griffin-Lim.")
 			generated_wav = Synthesizer.griffin_lim(spec)
 			
 		audio_length = librosa.get_duration(generated_wav, sr = self.synthesizer.sample_rate)
