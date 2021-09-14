a9ced5c00706a4d690980fd9564b49b939abfc77
padmalcom <j.freiknecht@googlemail.com>
2021-09-14 11:02:34 +0200

    Worked on intent classification

--- code/100_extras/depparsing/dependency_parsing.py
+++
@@ -1,8 +0,0 @@
-import spacy
-from spacy import displacy
-
-nlp = spacy.load("de_core_news_sm")
-doc = nlp("Goldfische sind pflegeleicht.")
-print(spacy.explain('sb'))
-print(spacy.explain('pd'))
-displacy.serve(doc, style="dep")
