42e1c7e6aa483f213df32c8f36f754642adf1874
padmalcom <j.freiknecht@googlemail.com>
2021-10-12 22:07:49 +0200

    Intent Classifier und Intent Parser angepasst.

--- code/100_extras/100_04_intentclassifier/main.py
+++ code/100_extras/100_04_intentclassifier/main.py
@@ -1,30 +1,34 @@
 import spacy
 from spacy.util import minibatch, compounding
-import time
-import random
-from sklearn.metrics import classification_report
-import os
-import string
+import time, random, os
+from sklearn.metrics import classification_report, confusion_matrix, ConfusionMatrixDisplay
 import pandas as pd
 from sklearn.utils import shuffle
 import numpy as np
 
-#https://github.com/rsreetech/TextClassificationWithSpacy/blob/master/TweetTextClassificationWithSpacy.ipynb
-def Sort(sub_li): 
-	return(sorted(sub_li, key = lambda x: x[1],reverse=True))  
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams.update({'figure.autolayout': True})
 
-# run the predictions on each sentence in the evaluation  dataset, and return the metrics
+# Es werden Vorhersagen für den Evaluierungsdatensatz getroffen und die
+# Metriken ausgegeben.
 def evaluate(tokenizer, textcat, test_texts, test_cats):
 	docs = (tokenizer(text) for text in test_texts)
 	preds = []
 	for i, doc in enumerate(textcat.pipe(docs)):
-		scores = Sort(doc.cats.items())
+		scores = sorted(doc.cats.items(), key = lambda x: x[1],reverse=True)
 		catList=[]
 		for score in scores:
 			catList.append(score[0])
 		preds.append(catList[0])
 		
 	labels = ["AddToPlaylist", "BookRestaurant", "GetWeather", "PlayMusic", "RateBook", "SearchCreativeWork", "SearchScreeningEvent"]
+	cm = confusion_matrix(test_cats, preds, labels=labels)
+	ConfusionMatrixDisplay(cm, display_labels=labels).plot()
+	plt.xticks(rotation=90)
+	plt.xlabel("Vorhergesagte Klasse")
+	plt.ylabel("Tatsächliche Klasse")
+	plt.show()
 	
 	print(classification_report(test_cats, preds, labels=labels))
 	
@@ -41,14 +45,13 @@ def load():
 	
 	return train_set, test_set
 
-def train(train_data, iterations, test_texts,test_cats, model_arch, dropout = 0.3, model=None,init_tok2vec=None):
+def train(train_data, iterations, test_texts, test_cats, dropout = 0.3):
 	nlp = spacy.load("de_core_news_sm")
 	
-	# add the text classifier to the pipeline if it doesn't exist
-	textcat = nlp.create_pipe("textcat", config={"exclusive_classes": True, "architecture": model_arch})
+	textcat = nlp.create_pipe("textcat", config={"exclusive_classes": True, "architecture": "ensemble"})
 	nlp.add_pipe(textcat, last=True)
 
-	# add label to text classifier
+	# Festlegen der Intents als Labels für den Classifier
 	textcat.add_label("AddToPlaylist")
 	textcat.add_label("BookRestaurant")
 	textcat.add_label("GetWeather")
@@ -57,39 +60,36 @@ def train(train_data, iterations, test_texts,test_cats, model_arch, dropout = 0.
 	textcat.add_label("SearchCreativeWork")
 	textcat.add_label("SearchScreeningEvent")
 
-	# get names of other pipes to disable them during training
+	# Nur die Pipeline für den Text Classifier wird benötigt.
 	pipe_exceptions = ["textcat", "trf_wordpiecer", "trf_tok2vec"]
 	other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
-	with nlp.disable_pipes(*other_pipes):  # only train textcat
+	with nlp.disable_pipes(*other_pipes):
 		optimizer = nlp.begin_training()
-		if init_tok2vec is not None:
-			with init_tok2vec.open("rb") as file_:
-				textcat.model.tok2vec.from_bytes(file_.read())
-		print("Training the model...")
-		#print("{:^5}\t{:^5}\t{:^5}\t{:^5}".format("LOSS", "P", "R", "F"))
-		batch_sizes = compounding(16.0, 64.0, 1.5)
+
+		print("Beginne Training ...")
+		#batch_sizes = compounding(16.0, 64.0, 1.5)
 		for i in range(iterations):
 			print('Iteration: '+str(i))
 			start_time = time.perf_counter()
 			losses = {}
-			# batch up the examples using spaCy's minibatch
 			random.shuffle(train_data)
-			batches = minibatch(train_data, size=batch_sizes)
+			#batches = minibatch(train_data, size=batch_sizes)
+			batches = minibatch(train_data, size=16)
 			for batch in batches:
 				texts, annotations = zip(*batch)
 				nlp.update(texts, annotations, sgd=optimizer, drop=dropout, losses=losses)
 			with textcat.model.use_params(optimizer.averages):
-				# evaluate on the test data 
 				evaluate(nlp.tokenizer, textcat, test_texts, test_cats)
-			print ('Elapsed time '+str(time.perf_counter() - start_time)+  " seconds")
+				
+			print ("Iteration " + str(i) + ": " +str(time.perf_counter() - start_time)+  " Sekunden")
+			
 		with nlp.use_params(optimizer.averages):
-			modelName = model_arch+"_model"
+			modelName = "ensemble_model"
 			filepath = os.path.join(os.getcwd(), modelName)
 			nlp.to_disk(filepath)
 	return nlp
 	
 if __name__ == '__main__':
-
 	train_set, test_set = load()
 	train_texts = train_set['sentences'] 
 	train_cats = train_set['intents']
@@ -112,7 +112,7 @@ if __name__ == '__main__':
 	test_texts = test_set['sentences'] 
 	test_cats = test_set['intents']
 	
-	nlp = train(training_data, 10, test_texts, test_cats, "ensemble")
+	nlp = train(training_data, 10, test_texts, test_cats)
 	
 	test1 = "Buche ein Restaurant für heute abend".lower()
 	test2 = "Füge alle Musik von Blink 182 meiner Playlist hinzu".lower()
--- code/100_extras/100_05_intentparser/main.py
+++ code/100_extras/100_05_intentparser/main.py
@@ -1,5 +1,3 @@
-#from __future__ import unicode_literals, print_function
-
 # https://github.com/explosion/spaCy/blob/v2.3.x/examples/training/train_intent_parser.py
 
 import plac
@@ -7,153 +5,155 @@ import random
 from pathlib import Path
 import spacy
 from spacy.util import minibatch, compounding
+from spacy import displacy
 
 
 # Trainingsdaten bestehend aus Text, Token Heads (das Wort auf, das sich dieses Wort bezieht) und syntaktischer Abhängikeit (Toeken Dependency)
 # "-" wird eingesetzt, falls es keine Beziehung gibt.
 TRAIN_DATA = [
 
-    (
-        "finde einen urlaubsort mit viel sonne",
-        {
-            "heads": [0, 2, 0, 5, 5, 2],
-            "deps": ["ROOT", "-", "PLACE", "-", "QUALITY", "ATTRIBUTE"],
-        },
-    ),
-	
-    (
-        "suche ein hotel nahe einer strandbar",
-        {
-            "heads": [0, 2, 0, 5, 5, 2],
-            "deps": ["ROOT", "-", "PLACE", "QUALITY", "-", "ATTRIBUTE"],
-        },
-    ),
-	
-    (
-        "finde ein appartment mit meerblick",
-        {
-            "heads": [0, 2, 0, 4, 2],
-            "deps": ["ROOT", "-", "PLACE", "-", "ATTRIBUTE"],
-        },
-    ),
+	(
+		"Finde einen Urlaubsort mit viel Sonne",
+		{
+			"heads": [0, 2, 0, 5, 5, 2],
+			"deps": ["ROOT", "-", "PLACE", "-", "QUALITY", "ATTRIBUTE"],
+		},
+	),
 	
 	(
-        "suche ein restaurant mit italienischem Essen",
-        {
-            "heads": [0, 2, 0, 5, 5, 2],
-            "deps": ["ROOT", "-", "PLACE", "-", "ATTRIBUTE", "PRODUCT"],
-        },
-    ),
+		"Suche ein Hotel nahe einer Strandbar",
+		{
+			"heads": [0, 2, 0, 5, 5, 2],
+			"deps": ["ROOT", "-", "PLACE", "QUALITY", "-", "ATTRIBUTE"],
+		},
+	),
 	
 	(
-        "finde eine tankstelle die blumen verkauft",
-        {
-            "heads": [0, 2, 0, 2, 2, 2],
-            "deps": ["ROOT", "-", "PLACE", "-", "PRODUCT", "-"],
-        },
-    ),
+		"Finde ein Appartment mit Meerblick",
+		{
+			"heads": [0, 2, 0, 4, 2],
+			"deps": ["ROOT", "-", "PLACE", "-", "ATTRIBUTE"],
+		},
+	),
 	
 	(
-        "finde ein gutes veganes cafe nahe des parks",
-        {
-            "heads": [0, 4, 4, 4, 0, 7, 7, 4],
-            "deps": ["ROOT", "-", "QUALITY", "ATTRIBUTE", "PLACE", "ATTRIBUTE", "-", "LOCATION"],
-        },
-    ),
+		"Suche ein Restaurant mit italienischem Essen",
+		{
+			"heads": [0, 2, 0, 5, 5, 2],
+			"deps": ["ROOT", "-", "PLACE", "-", "ATTRIBUTE", "PRODUCT"],
+		},
+	),
+	
+	(
+		"Finde eine Tankstelle die Blumen verkauft",
+		{
+			"heads": [0, 2, 0, 2, 2, 2],
+			"deps": ["ROOT", "-", "PLACE", "-", "PRODUCT", "-"],
+		},
+	),
+	
+	(
+		"Finde ein gutes veganes Cafe nahe des Parks",
+		{
+			"heads": [0, 4, 4, 4, 0, 7, 7, 4],
+			"deps": ["ROOT", "-", "QUALITY", "ATTRIBUTE", "PLACE", "ATTRIBUTE", "-", "LOCATION"],
+		},
+	),
 ]
 
 
 @plac.annotations(
-    model=("Modellname (default 'de' Modell)", "option", "m", str),
-    output_dir=("Ausgabepfad", "option", "o", Path),
-    n_iter=("Interationen des Trainings", "option", "n", int),
+	model=("Modellname (default 'de' Modell)", "option", "m", str),
+	output_dir=("Ausgabepfad", "option", "o", Path),
+	n_iter=("Interationen des Trainings", "option", "n", int),
 )
 def main(model=None, output_dir=None, n_iter=15):
-    """Load the model, set up the pipeline and train the parser."""
-    if model is not None:
-        nlp = spacy.load(model)  # load existing spaCy model
-        print("Loaded model '%s'" % model)
-    else:
-        nlp = spacy.blank("de")  # create blank Language class
-        print("Created blank 'de' model")
+	"""Load the model, set up the pipeline and train the parser."""
+	if model is not None:
+		nlp = spacy.load(model)  # load existing spaCy model
+		print("Loaded model '%s'" % model)
+	else:
+		nlp = spacy.blank("de")  # create blank Language class
+		print("Created blank 'de' model")
 
-    # We'll use the built-in dependency parser class, but we want to create a
-    # fresh instance – just in case.
-    if "parser" in nlp.pipe_names:
-        nlp.remove_pipe("parser")
-    parser = nlp.create_pipe("parser")
-    nlp.add_pipe(parser, first=True)
+	# We'll use the built-in dependency parser class, but we want to create a
+	# fresh instance – just in case.
+	if "parser" in nlp.pipe_names:
+		nlp.remove_pipe("parser")
+	parser = nlp.create_pipe("parser")
+	nlp.add_pipe(parser, first=True)
 
-    for text, annotations in TRAIN_DATA:
-        for dep in annotations.get("deps", []):
-            parser.add_label(dep)
+	for text, annotations in TRAIN_DATA:
+		for dep in annotations.get("deps", []):
+			parser.add_label(dep)
 
-    pipe_exceptions = ["parser", "trf_wordpiecer", "trf_tok2vec"]
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
-    with nlp.disable_pipes(*other_pipes):  # only train parser
-        optimizer = nlp.begin_training()
-        for itn in range(n_iter):
-            random.shuffle(TRAIN_DATA)
-            losses = {}
-            # batch up the examples using spaCy's minibatch
-            batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
-            for batch in batches:
-                texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer, losses=losses)
-            print("Losses", losses)
+	pipe_exceptions = ["parser", "trf_wordpiecer", "trf_tok2vec"]
+	other_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+	with nlp.disable_pipes(*other_pipes):  # only train parser
+		optimizer = nlp.begin_training()
+		for itn in range(n_iter):
+			random.shuffle(TRAIN_DATA)
+			losses = {}
+			# batch up the examples using spaCy's minibatch
+			batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
+			for batch in batches:
+				texts, annotations = zip(*batch)
+				nlp.update(texts, annotations, sgd=optimizer, losses=losses)
+			print("Losses", losses)
 
-    # test the trained model
-    test_model(nlp)
+	# test the trained model
+	test_model(nlp)
 
-    # save model to output directory
-    if output_dir is not None:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
-        nlp.to_disk(output_dir)
-        print("Saved model to", output_dir)
+	# save model to output directory
+	if output_dir is not None:
+		output_dir = Path(output_dir)
+		if not output_dir.exists():
+			output_dir.mkdir()
+		nlp.to_disk(output_dir)
+		print("Saved model to", output_dir)
 
-        # test the saved model
-        print("Loading from", output_dir)
-        nlp2 = spacy.load(output_dir)
-        test_model(nlp2)
+		# test the saved model
+		print("Loading from", output_dir)
+		nlp2 = spacy.load(output_dir)
+		test_model(nlp2)
 
 
 def test_model(nlp):
-    texts = [
-        "finde ein hotel mit gutem internet",
-        "suche das günstiges fitnessstudio nahe der arbeit",
-        "suche das beste restaurant in berlin",
-    ]
-    docs = nlp.pipe(texts)
-    for doc in docs:
-        print(doc.text)
-        print([(t.text, t.dep_, t.head.text) for t in doc if t.dep_ != "-"])
+	texts = [
+		"Finde einen Urlaubsort mit viel Sonne",
+		"suche das günstiges fitnessstudio nahe der arbeit",
+		"suche das beste restaurant in berlin",
+	]
+	docs = nlp.pipe(texts)
+	for doc in docs:
+		print(doc.text)
+		print([(t.text, t.dep_, t.head.text) for t in doc if t.dep_ != "-"])
+		displacy.serve(doc, style="dep")
 
 
 if __name__ == "__main__":
-    plac.call(main)
+	plac.call(main)
 
-    # Expected output:
-    # find a hotel with good wifi
-    # [
-    #   ('find', 'ROOT', 'find'),
-    #   ('hotel', 'PLACE', 'find'),
-    #   ('good', 'QUALITY', 'wifi'),
-    #   ('wifi', 'ATTRIBUTE', 'hotel')
-    # ]
-    # find me the cheapest gym near work
-    # [
-    #   ('find', 'ROOT', 'find'),
-    #   ('cheapest', 'QUALITY', 'gym'),
-    #   ('gym', 'PLACE', 'find'),
-    #   ('near', 'ATTRIBUTE', 'gym'),
-    #   ('work', 'LOCATION', 'near')
-    # ]
-    # show me the best hotel in berlin
-    # [
-    #   ('show', 'ROOT', 'show'),
-    #   ('best', 'QUALITY', 'hotel'),
-    #   ('hotel', 'PLACE', 'show'),
-    #   ('berlin', 'LOCATION', 'hotel')
-    # ]
+	# Expected output:
+	# find a hotel with good wifi
+	# [
+	#   ('find', 'ROOT', 'find'),
+	#   ('hotel', 'PLACE', 'find'),
+	#   ('good', 'QUALITY', 'wifi'),
+	#   ('wifi', 'ATTRIBUTE', 'hotel')
+	# ]
+	# find me the cheapest gym near work
+	# [
+	#   ('find', 'ROOT', 'find'),
+	#   ('cheapest', 'QUALITY', 'gym'),
+	#   ('gym', 'PLACE', 'find'),
+	#   ('near', 'ATTRIBUTE', 'gym'),
+	#   ('work', 'LOCATION', 'near')
+	# ]
+	# show me the best hotel in berlin
+	# [
+	#   ('show', 'ROOT', 'show'),
+	#   ('best', 'QUALITY', 'hotel'),
+	#   ('hotel', 'PLACE', 'show'),
+	#   ('berlin', 'LOCATION', 'hotel')
+	# ]
