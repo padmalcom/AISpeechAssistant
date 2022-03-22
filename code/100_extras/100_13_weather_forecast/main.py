e54c1b0c6c7dd1da8919c6020fd115bdc25c3400
padmalcom <j.freiknecht@googlemail.com>
2022-03-22 13:56:47 +0100

    Cleaned weather forecast

--- code/100_extras/100_13_weather_forecast/main.py
+++ code/100_extras/100_13_weather_forecast/main.py
@@ -1,34 +1,26 @@
-# Source = https://keras.io/examples/timeseries/timeseries_weather_forecasting/
-
-from loguru import logger
+import os
+import csv
+from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationPeriod, DwdObservationResolution, DwdObservationDataset
 from datetime import datetime, timedelta
 import pandas as pd
-import matplotlib.pyplot as plt
-import os
-import json
+import numpy as np
+from matplotlib import pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
-import numpy as np
+from tensorflow.keras import layers
 from tabulate import tabulate
+from loguru import logger
 
-from wetterdienst.provider.dwd.observation import DwdObservationRequest, DwdObservationPeriod, DwdObservationResolution, DwdObservationDataset
+# only display tensorflow error messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-WEATHER_FILE = 'weather_data.csv'
-PARAMETERS_FILE = 'parameters.csv'
+WEATHER_DATA = os.path.join("weather_data.csv")
 
-feature_names = ['temperature_air_mean_200']#, 'humidity']
-
-USE_CACHE = False
-
-colors = ["blue", "orange", "green", "red",	"purple", "brown", "pink", "gray", "olive", "cyan"]
-
-# Training parameter
-sampling_rate = 6									# sample six data points (6*10 minutes = 1h) to one
-sequence_length = 120								# look at 5 days
-batch_size = 256
-learning_rate = 0.001
-epochs=1
-outlook=sampling_rate * (sequence_length + 24 - 1)
+SAMPLING_RATE = 6
+SEQUENCE_LENGTH = 120
+OUTLOOK = SAMPLING_RATE * (SEQUENCE_LENGTH + 24*3 - 1) # 3 days in the future
+BATCH_SIZE = 256
+EPOCHS = 15
 
 def get_station():
 
@@ -48,7 +40,7 @@ def get_station():
 	if (len(stations)) > 0:
 		return stations.iloc[0]
 	return None
-
+	
 def get_data(station):
 
 	start_date = (datetime.now() - timedelta(days=1*365)).strftime('%Y-%m-%d')
@@ -64,11 +56,7 @@ def get_data(station):
 		end_date=end_date
 	)
 	station_data = request.filter_by_station_id(station['station_id']).values.all().df
-	
-	parameters =  pd.unique(station_data['parameter']).tolist()
-	with open(PARAMETERS_FILE, 'w') as f:
-		json.dump(parameters, f, indent=2) 
-					
+	parameters =  pd.unique(station_data['parameter']).tolist()					
 	return station_data, parameters
 	
 def preprocess_data(station_data, parameters):
@@ -89,241 +77,191 @@ def preprocess_data(station_data, parameters):
 	station_data = station_data.groupby('date').aggregate(aggregegation_functions)
 		
 	return station_data
-	
-def plot_raw_data(station_data, parameters):
-	fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(10, 10), dpi=80, facecolor="w", edgecolor="k")
-	for i in range(len(parameters)):
-		key = parameters[i]
-		c = colors[i % (len(colors))]
-		t_data = station_data[key]
-		t_data.index = station_data['date']
-		t_data.head()
-		ax = t_data.plot(
-			ax=axes[i // 2, i % 2],
-			color=c,
-			title="{} - {}".format(parameters[i], key),
-			rot=25,
-		)
-		ax.legend([parameters[i]])
-	plt.tight_layout()
-	plt.show()
-	
-def plot_correlation(station_data):
-    plt.matshow(station_data.corr())
-    plt.xticks(range(station_data.shape[1]), station_data.columns, fontsize=8, rotation=90)
-    plt.gca().xaxis.tick_bottom()
-    plt.yticks(range(station_data.shape[1]), station_data.columns, fontsize=8)
 
-    cb = plt.colorbar()
-    cb.ax.tick_params(labelsize=6)
-    plt.title("Feature Correlation Heatmap", fontsize=10)
-    plt.show()
+def read_data():
+	temperature = []
+	raw_data = []
+	dates = []
+	with open(WEATHER_DATA, newline='') as csvfile:
+		spamreader = csv.reader(csvfile, delimiter=',', quotechar='\'')
+			
+		for i, row in enumerate(spamreader):
+			if i==0:
+				continue
+			row[2] = float(row[2]) - 273.15 # kelvin to celsius
+			temperature.append(float(row[2]))
+			raw_data.append([float(x) for x in row[2:6]])
+			dates.append(row[0])
+			
+	logger.info("Raw data (first 5 entries):\n{}", raw_data[:5])
+	logger.info("Temperature (first 5 entries):\n{}", temperature[:5])
+
+	num_train_samples = int(0.7 * len(raw_data))
+	num_val_samples = int(0.2 * len(raw_data))
 	
-def prepare_training_and_validation_data(station_data, parameters):
-	#train_split = int(split_fraction * int(station_data.shape[0]))
-	num_training_data = int(0.5 * station_data.shape[0])
-	num_validation_data = int(0.25 * station_data.shape[0])
-	num_test_data = station_data.shape[0] - num_training_data - num_validation_data
-	
-	logger.info("Station data length (in days): {}", station_data.shape[0])
-	logger.info("Training data: {}, validation data: {}, test data: {}", num_training_data, num_validation_data, num_test_data)
+	logger.info("Number of training samples: {}, number of evaluation samples: {}, remaining samples for prediction: {}",
+		num_train_samples,
+		num_val_samples,
+		num_train_samples - num_val_samples
+	)
+	return raw_data, temperature, dates, num_train_samples, num_val_samples
 
-	# Feature value ranges differ, so standardize (form of normalization)
-	def standardize(data, num_training_data):
-		data_mean = data[:num_training_data].mean(axis=0) # using only training data does not influence val + test
-		data_std = data[:num_training_data].std(axis=0)
-				
-		# return mean and std for inverse standardization
-		return (data - data_mean) / data_std, data_mean[0], data_std[0] # Index 0 for temperature
-		
-	logger.info("Selected parameters are: {}", ", ".join(feature_names))
-		
-	features = station_data[feature_names]
-	features.index = station_data['date']
-	logger.info("Indexed data with selected features:\n{}", tabulate(features.head(), headers='keys'))
+def standardize(raw_data, temperature):
+	raw_data = np.array(raw_data[:])
+	mean = raw_data[:num_train_samples].mean(axis=0)
+	std = raw_data[:num_train_samples].std(axis=0)
+	logger.info("Standardizing data with mean {} and standard deviation {}.", mean, std)
+	raw_data = (raw_data - mean) / std
+	temperature = [(x - mean[0]) / std[0] for x in temperature]
+	return raw_data, temperature, mean, std
 
-	features, mean, std = standardize(features.values, num_training_data)
-	features = pd.DataFrame(features)
-	logger.info("Normalized data:\n{}", tabulate(features.head(), headers='keys'))
-
-	dataset_train = keras.preprocessing.timeseries_dataset_from_array(
-		features[:-outlook],#x_train,
-		features[outlook:][0],#y_train,
-		sequence_length=sequence_length,
-		sampling_rate=sampling_rate,
-		batch_size=batch_size,
+def create_datasets(raw_data, temperature, num_train_samples, num_val_samples):
+	train_dataset = keras.utils.timeseries_dataset_from_array(
+		raw_data[:-OUTLOOK],
+		targets=temperature[OUTLOOK:],
+		sampling_rate=SAMPLING_RATE,
+		sequence_length=SEQUENCE_LENGTH,
+		shuffle=True,
+		batch_size=BATCH_SIZE,
 		start_index=0,
-		end_index=num_training_data-1
-	)
+		end_index=num_train_samples)
 
-	dataset_val = keras.preprocessing.timeseries_dataset_from_array(
-		features[:-outlook],#x_train,
-		features[outlook:][0],#y_train,
-		sequence_length=sequence_length,
-		sampling_rate=sampling_rate,
-		batch_size=batch_size,
-		start_index=num_training_data,
-		end_index=num_training_data + num_validation_data-1
-	)
+	val_dataset = keras.utils.timeseries_dataset_from_array(
+		raw_data[:-OUTLOOK],
+		targets=temperature[OUTLOOK:],
+		sampling_rate=SAMPLING_RATE,
+		sequence_length=SEQUENCE_LENGTH,
+		shuffle=True,
+		batch_size=BATCH_SIZE,
+		start_index=num_train_samples,
+		end_index=num_train_samples + num_val_samples - 1)
 		
-	dataset_test = keras.preprocessing.timeseries_dataset_from_array(
-		features[:-outlook],
-		None,
-		sequence_length=sequence_length,
-		sampling_rate=sampling_rate,
-		batch_size=batch_size,
-		start_index=num_training_data + num_validation_data
-	)
-	
-	for batch in dataset_train.take(1):
-		inputs, targets = batch
-
-		print("Input shape:", inputs.numpy().shape)
-		print("Target shape:", targets.numpy().shape)
-
-	return dataset_train, dataset_val, dataset_test, mean, std
-
-def train(dataset_train, dataset_val):
-	for batch in dataset_train.take(1):
-		inputs, targets = batch
-
-	logger.info("Input shape: {}", inputs.numpy().shape)
-	logger.info("Target shape: {}", targets.numpy().shape)
+	for samples, targets in train_dataset.take(1):
+		logger.info("Samples shape: {} (num samples, sequence length, num features)", samples.shape)
+		logger.info("targets shape: {} (normalized temperature)", targets.shape)
 		
-	#inputs = keras.layers.Input(shape=(inputs.shape[1], inputs.shape[2]))
-	#lstm_out = keras.layers.LSTM(32)(inputs)
-	#outputs = keras.layers.Dense(1)(lstm_out)
+	return train_dataset, val_dataset
 
-	#model = keras.Model(inputs=inputs, outputs=outputs)
-	#model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss="mse")
-	#model.summary()
-	inputs = keras.Input(shape=(sequence_length, inputs.shape[2]))
-	x = keras.layers.Bidirectional(keras.layers.LSTM(16))(inputs)
-	outputs = keras.layers.Dense(1)(x)
+def train(feature_count, train_dataset, val_dataset):
+	inputs = keras.Input(shape=(SEQUENCE_LENGTH, raw_data.shape[-1]))
+	x = layers.Bidirectional(layers.LSTM(32))(inputs)
+	outputs = layers.Dense(1)(x)
 	model = keras.Model(inputs, outputs)
 
 	model.compile(optimizer="rmsprop", loss="mse", metrics=["mae"])
-	history = model.fit(dataset_train,
-						epochs=epochs,
-						validation_data=dataset_val)
-	
-	#path_checkpoint = "model_checkpoint.h5"
-	#es_callback = keras.callbacks.EarlyStopping(monitor="val_loss", min_delta=0, patience=5)
+	history = model.fit(train_dataset, epochs=EPOCHS, validation_data=val_dataset)
+						
+	loss = history.history["mae"]
+	val_loss = history.history["val_mae"]
+	epochs = range(1, len(loss) + 1)
+	return model, loss, val_loss, epochs
 
-	#modelckpt_callback = keras.callbacks.ModelCheckpoint(
-	#	monitor="val_loss",
-	#	filepath=path_checkpoint,
-	#	verbose=1,
-	#	save_weights_only=True,
-	#	save_best_only=True,
-	#)
 
-	#history = model.fit(
-	#	dataset_train,
-	#	epochs=epochs,
-	#	validation_data=dataset_val,
-	#	callbacks=[es_callback, modelckpt_callback],
-	#	)
-	return model
+def visualize_training(loss, val_loss, epochs):
+	plt.figure()
+	plt.plot(epochs, loss, "bo", label="Training MAE")
+	plt.plot(epochs, val_loss, "b", label="Validation MAE")
+	plt.title("Training and validation MAE")
+	plt.legend()
+	plt.show()
 
-def plot_data_prediction(plot_data, delta, title):
+def visualize_evaluation(plot_data, delta, title):
 	labels = ["History", "True Future", "Model Prediction"]
 	marker = [".-", "rx", "go"]
 	time_steps = list(range(-(plot_data[0].shape[0]), 0))
-	
 	if delta:
 		future = delta
 	else:
 		future = 0
 
 	plt.title(title)
-	
-	print(plot_data)
-	
-	# Plot history (0), true future (1) and model prediction (2)
 	for i, val in enumerate(plot_data):
-	
-		print(i)
-		print(plot_data[i])
-		# Are we in prediction mode?
-		if i == 1 and not val.any():
-			continue
-			
-		if i:
-			print("i:", i, " len future: ", future, "len plot data", len(plot_data[i]))
-			future = [future] * len(plot_data[i])
-			print("i:", i, " len future: ", len(future), "len plot data", len(plot_data[i]))
-			plt.plot(future, plot_data[i], marker[i], markersize=10, label=labels[i])
-		else:
-			#plt.plot(time_steps, plot_data[i].flatten(), marker[i], label=labels[i])
-			pass
-			
+		if i: # i==1: ground_truth, i==2: prediction
+			plt.plot(future, plot_data[i][0,0], marker[i], markersize=10, label=labels[i])
+		else: # i== 0: history
+			plt.plot(time_steps, [x[0] for x in plot_data[i]], marker[i], label=labels[i])
 	plt.legend()
-	print("t1", type(time_steps[0]), "t2:", type((future + 5) * 2))
 	plt.xlim([time_steps[0], (future + 5) * 2])
 	plt.xlabel("Time-Step")
 	plt.show()
 
-	
-def inverse_standardization(value, mean, std):
-	return value * std + mean
-	
-def kelvin_to_celsius(value):
-	return value - 273.15
+def evaluate(val_dataset, model):
+	for x, y in val_dataset.take(1):
+		history = x[0][:, 0].numpy()
+		ground_truth = y[0].numpy()
+		prediction = model.predict(x)[0]
 
-def main():
-	if (not os.path.exists(WEATHER_FILE) or not USE_CACHE):
+		history = np.array([xi * std  + mean for xi in history])
+		ground_truth = np.array([ground_truth * std + mean])
+		prediction = np.array([prediction * std + mean])
+				
+		visualize_evaluation([history, ground_truth, prediction], 3, "Single Step Prediction") # 3 = 3 day in the future
+
+def predict(prediction_sequence):
+	prediction = model.predict(prediction_sequence)
+	prediction = np.array([prediction * std + mean])
+	prediction = prediction[0]
+	return prediction[0,0]
+
+if __name__ == '__main__':
+
+	if not os.path.exists(WEATHER_DATA):
 		station = get_station()
+		logger.info("Found station {}.", station)
 		data, parameters = get_data(station)
+		logger.info("Read data with {} entries.", len(data))
 		logger.info("Parameters: {}", parameters)
 		data = preprocess_data(data, parameters)
-		data.to_csv(WEATHER_FILE, index=False)
-	else:
-		data = pd.read_csv(WEATHER_FILE)
-		with open(PARAMETERS_FILE, 'r') as f:
-			parameters = json.load(f)
+		data.to_csv(WEATHER_DATA, index=False)
+		logger.info("Weather file {} written.", WEATHER_DATA)
 		
-	#plot_raw_data(data, parameters)
-	#plot_correlation(data)
-		
-	dataset_train, dataset_val, dataset_infer, mean, std = prepare_training_and_validation_data(data, parameters)
+	logger.info("GPUs Available: {}", len(tf.config.list_physical_devices('GPU')))
 	
-	print("train")
-	print(dataset_train)
+	raw_data, temperature, dates, num_train_samples, num_val_samples = read_data()
+	raw_data, temperature, mean, std = standardize(
+		raw_data,
+		temperature
+	)
+	train_dataset, val_dataset = create_datasets(
+		raw_data,
+		temperature,
+		num_train_samples,
+		num_val_samples
+	)
+	model, loss, val_loss, epochs = train(
+		raw_data.shape[-1],
+		train_dataset,
+		val_dataset
+	)
+	visualize_training(
+		loss,
+		val_loss,
+		epochs
+	)
+	evaluate(
+		val_dataset,
+		model
+	)
 	
-	model = train(dataset_train, dataset_val)
-		
-	for x, y in dataset_val.take(1):
-		print("x:", x)
-		print("y:", y)
-		#history = x[0][:, 0].numpy() # 0 for temperature
-		#ground_truth = y[0].numpy()
-		#history = x[0][:, 0].numpy()
-		history = x[0][:,0].numpy()
-		ground_truth = y.numpy()
-		prediction = model.predict(x)[0]
+	# hour that we want to predict the time for on the next day
+	prediction_hour = 12
 
-		print(history.shape)
-		print(ground_truth.shape)
-		print("pred: ", prediction)
+	# Create sequence for prediction from last 5 days (120 hours) to predict the next 72 hours
+	prediction_sequence = np.asarray(raw_data[-120-prediction_hour*6:-prediction_hour*6]) # need exactly sequence of 120 entries
+	
+	# Insert one dimension (# of samples)
+	prediction_sequence = np.expand_dims(prediction_sequence, axis=0) 
+	weather_in_24h_day = predict(prediction_sequence)
+	
+	# get the last date in the dataset and add 3 days
+	tomorrow = datetime.fromisoformat(dates[-1  - prediction_hour*6]) + timedelta(days=3)
 
-				
-		history = np.array([kelvin_to_celsius(inverse_standardization(xi, mean, std)) for xi in history])
-		ground_truth = np.array([kelvin_to_celsius(inverse_standardization(xi, mean, std)) for xi in ground_truth])
-		prediction = [kelvin_to_celsius(inverse_standardization(xi, mean, std)) for xi in prediction]
-						
-		plot_data_prediction([history, ground_truth, prediction], 12, "Validation")
-		
-	# predict the actual temperature
-	for x in dataset_infer.take(1):
-		history = x[0][:, 0].numpy() # 0 for temperature
-		#ground_truth = y[0].numpy() # We don't have a ground truth
-		prediction = model.predict(x)[0]
-				
-		history = np.array([kelvin_to_celsius(inverse_standardization(xi, mean, std)) for xi in history])
-		prediction = np.array([kelvin_to_celsius(inverse_standardization(xi, mean, std)) for xi in prediction])
-						
-		plot_data_prediction([history, None, prediction], 12, "Prediction")		
-			
-if __name__ == "__main__":
-	main()
+	logger.info("Temperature at {} is {}.", tomorrow, weather_in_24h_day)
+	
+	
+	
+	
+	
+	
+	
+	
